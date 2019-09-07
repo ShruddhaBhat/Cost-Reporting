bbde19d6ec2d89bb6b919bd6583ee1365d48794a
Shruddha <bhat.shruddha@gmail.com>
2019-09-07 20:40:42 +0530

    Uploaded the files containing code and sample outputs

--- On-Demand_Cost_Reduction_Recommendations.md
+++ On-Demand_Cost_Reduction_Recommendations.md
@@ -1,7 +1,9 @@
-Problem Statement - "Based on the instance types and on demand costs put forward some recommendations on how to increase the RI coverage."
+# Problem Statement - "Based on the instance types and on demand costs put forward some recommendations on how to increase the RI coverage."
 
+`````````````
 The solutions or the recommendations for reducing the on-demand cost and increasing the RI coverage depends on individual use cases. For the given data in ri-instanceType.csv, below are few observations which can bring down on-demand cost - 
     1. We notice that there are few instances which are being run for 1000+ on-demand hours. Running such (non-temporary)     instances as RI, we can bring down the on-demand costs. 
     2. Depending on the requirement, by deploying right sized instances and in regions where the chosen instance type comes with less price can bring down the on-demand cost to an extent.
     3. Going for convertible reserved instances, we would be able to increase or decreasr the instance size or exhange it with another convertible instance, thereby helping to achieve scalability of instance size.
     4. Buying reserved instances with full or partial upfront cost for long running instances, the on-demand cost can be significantly brought down.
+`````````````````
---
+++ instance-price.py
@@ -0,0 +1,50 @@
+#!/usr/local/bin/python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+
+##Read csv as data frame
+data = pd.read_csv('ri-instanceTypes.csv')
+
+data = data[data['On-Demand Hours'] != 0]
+
+##Calculate per hour cost
+new_data = data.assign(Hourly_Cost = lambda x: data['On-Demand Cost']/data['On-Demand Hours'])
+
+##Find the max and the min priced instances based on hourly cost
+mostExpensive = new_data.loc[new_data['Hourly_Cost'].idxmax()]
+print('\n'"------- Most Expensive Instance --------"'\n')
+print(mostExpensive.to_string())
+
+leastExpensive = new_data.loc[new_data['Hourly_Cost'].idxmin()]
+print('\n'"------- Least Expensive Instance --------"'\n')
+print(leastExpensive.to_string())
+
+##Finding the highest and least priced instance in every region and process the data frame for graph plot
+grouped = new_data.groupby('Region')
+idx = new_data.groupby(['Region'])['Hourly_Cost'].transform(max) == new_data['Hourly_Cost']
+idn = new_data.groupby(['Region'])['Hourly_Cost'].transform(min) == new_data['Hourly_Cost']
+print('\n\n\n')
+
+filtered1 = pd.DataFrame(new_data[idx], columns=['Region', 'Instance Type', 'Hourly_Cost'])
+filtered2 = pd.DataFrame(new_data[idn], columns=['Region', 'Instance Type', 'Hourly_Cost'])
+
+frame1 = filtered1.sort_values('Region', ascending=True)
+frame2 = filtered2.sort_values('Region', ascending=True)
+
+print('\n\n')
+
+##Plot the graph using the pre-processed data above
+fig, ax = plt.subplots()
+
+frame1Plot = ax.bar(frame1['Region'], frame1['Hourly_Cost'], label='Most Expensive', color='Orange')
+frame2Plot = ax.bar(frame2['Region'], frame2['Hourly_Cost'], label='Least Expensive', color='Green')
+
+plt.xlabel('AWS Regions')
+plt.ylabel('Price Per Hour')
+plt.legend()
+ax.set_title('Most and least expensive instance in every AWS region')
+plt.show()
+plt.savefig('price-of-instances.png', bbox_inches='tight')
